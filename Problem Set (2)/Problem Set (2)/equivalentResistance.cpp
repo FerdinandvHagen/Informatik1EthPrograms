26ad55cc0809f729774f821c48bbb7118cb2aee0
Ferdinand von Hagen <ferdinand.vonhagen@gmail.com>
2014-09-24 09:45:38 +0200

    solved Problem Set (2)

---
+++ Problem Set (2)/Problem Set (2)/equivalentResistance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main(void){
+	int r1;
+	int r2;
+	int r3;
+	int r4;
+
+	std::cout << "Enter values for R1, R2, R3 and R4:" << std::endl;
+	std::cin >> r1 >> r2 >> r3 >> r4;
+
+
+	// Compute the serial resistance of each branch
+	int r12 = r1 + r2;
+	int r34 = r3 + r4;
+
+
+	/*
+	This would result in a "division by zero" exception:
+
+	int rg = 1 / ((1 / r12) + (1 / r34));
+	*/
+
+	//So correct would be:
+	int rg = r12 * r34 / (r12 + r34);
+	std::cout << "The correct equivalent resistance should be " << rg << std::endl;
+
+	std::cin >> rg;
+
+	return 0;
+}
