678a7e521563afe80accdb572a8bae54872a69f9
Ferdinand von Hagen <ferdinand.vonhagen@gmail.com>
2014-10-13 13:08:09 +0200

    Added Problems

--- Problem Set (3)/Problem Set (3)/dec2bin.cpp
+++ Problem Set (3)/Problem Set (3)/dec2bin.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <cassert>
 
 void recdec2bin(int n);
+void problem34(void);
+void problem34b(void);
+bool isPrime(int n);
+void primesUntilOverflow(void);
 
 int main(void){
 	std::cout << "Please input an integer: ";
@@ -10,6 +15,13 @@ int main(void){
 	recdec2bin(n);
 	std::cout << std::endl;
 
+	problem34();
+
+	problem34b();
+
+	std::cout << std::endl << std::endl << "Primes until overflow";
+	primesUntilOverflow();
+
 	std::cin >> n;
 	return 0;
 }
@@ -18,5 +30,65 @@ void recdec2bin(int n){
 	if (n != 0){
 		recdec2bin(n / 2);
 		std::cout << n % 2;
+	}
+}
+
+void problem34(void){
+	std::cout << "Please enter a number: ";
+	int n;
+	std::cin >> n;
+
+	int left = 0;
+	int right = 1;
+	std::cout << "0" << std::endl;
+	while (right <= n){
+		if (isPrime(right)){
+			std::cout << right << std::endl;
+		}
+		int res = left + right;
+		left = right;
+		right = res;
+	}
+}
+
+void primesUntilOverflow(void){
+	for (int n = 0; n < 100; n++){
+		if (isPrime(n)){
+			std::cout << n << std::endl;
+		}
+	}
+}
+
+void problem34b(void){
+	unsigned long left = 0u;
+	unsigned long right = 1u;
+	std::cout << "0" << std::endl;
+	while (right >= left){
+		if (isPrime(right)){
+			std::cout << right << std::endl;
+		}
+		int res = left + right;
+		left = right;
+		right = res;
+	}
+	std::cout << "problem 3.4 b solved: overflow after fibonacci prime " << left << " because the next value was " << right << std::endl;
+	assert(right < left);
+}
+
+bool isPrime(int n){
+	if (n <= 1){
+		return false;
+	}
+	unsigned int d;
+	for (d = 2; n % d != 0; ++d);
+
+	if (d < n){
+		//d is a divisor of n in {2, ..., n-1}
+		return false;
+	}
+	else {
+		assert(d == n);
+		//there is no d in {2, ..., n-1} to divide n
+		return true;
 	}
 }
