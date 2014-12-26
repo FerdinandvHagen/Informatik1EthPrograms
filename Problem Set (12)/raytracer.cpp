637694f87e8e19f1f6165aa55ac5c68db9f8108f
Ferdinand von Hagen <ferdinand.vonhagen@gmail.com>
2014-12-26 10:33:27 +0100

    changed RayTracer

--- Problem Set (12)/raytracer.cpp
+++ Problem Set (12)/raytracer.cpp
@@ -1,51 +1,55 @@
-//Exercise 12
-//Author: Ferdinand von Hagen
-//LEGI: 14-942-270
-//Date: 2014/12/07
-
-
-#include <cmath>
-#include <cassert>
-#include <algorithm>
 #include <iostream>
+#include <cmath>
+#include <limits>
 #include "bitmap.cpp"
 
-//PART A
+//Part A
+
 struct Vector
 {
-    //POST: vector should be initialized as null-vector
     double x;
     double y;
     double z;
 
-    Vector() :
-        x(0.0), y(0.0), z(0.0) {};
+    Vector()
+    {
+        x = 0.0;
+        y = 0.0;
+        z = 0.0;
+    }
 
-    //POST: coordinates (x,y,z) set to (x_, y_, z_)
-    Vector(double x_, double y_, double z_) :
-        x(x_), y(y_), z(z_) {};
+    Vector(double x_, double y_, double z_)
+    {
+        x = x_;
+        y = y_;
+        z = z_;
+    }
 };
 
-//POST: Returns the negation of the vector
-Vector operator-(Vector vec)
+Vector operator-(const Vector &v)
 {
-    return Vector(-vec.x, -vec.y, -vec.z);
+    return Vector(-v.x, -v.y, -v.z);
 }
 
-//POST: Adds two vectors
-Vector operator+(Vector v, Vector w)
+Vector operator-(const Vector &v, const Vector &w)
 {
-    return Vector(v.x + w.x, v.y + w.y, v.z + w.z);
+    return Vector(v.x-w.x, v.y-w.y, v.z-w.z);
 }
 
-//POST: Subtracts one vector of another (v-w)
-Vector operator-(Vector v, Vector w)
+Vector operator+(const Vector &v, const Vector &w)
 {
-    return v+-w;
+    return Vector(v.x+w.x, v.y+w.y, v.z+w.z);
 }
 
-//POST: Subtracts two vectors and stores the result in the minuend
-Vector& operator+=(Vector& v, Vector w)
+Vector& operator-=(Vector &v, const Vector &w)
+{
+    v.x -= w.x;
+    v.y -= w.y;
+    v.z -= w.z;
+    return v;
+}
+
+Vector& operator+=(Vector &v, const Vector &w)
 {
     v.x += w.x;
     v.y += w.y;
@@ -53,20 +57,12 @@ Vector& operator+=(Vector& v, Vector w)
     return v;
 }
 
-//POST: Subtracts two vectors and stores the result in the minuend
-Vector& operator-=(Vector& v, Vector w)
+double operator*(const Vector &v, const Vector &w)
 {
-    return v += -w;
+    return v.x*w.x+v.y*w.y+v.z*w.z;
 }
 
-//POST: Scalar product of two Vectors
-double operator*(Vector v, Vector w)
-{
-    return (v.x*w.x) + (v.y*w.y) + (v.z*w.z);
-}
-
-//POST: Scalar multiplication of a vector with a scalar
-Vector& operator*=(Vector& v, double r)
+Vector& operator *= (Vector &v, const double &r)
 {
     v.x *= r;
     v.y *= r;
@@ -74,233 +70,196 @@ Vector& operator*=(Vector& v, double r)
     return v;
 }
 
-//POST: Scalar multiplication of a vector with a scalar
-Vector operator*(Vector v, double r)
+Vector operator*(const Vector &v, const double &r)
 {
-    return Vector(v.x * r, v.y * r, v.z * r);
+    return Vector(v.x*r, v.y*r, v.z*r);
 }
 
-//POST: Scalar multiplication of a vector with a scalar
-Vector operator*(double r, Vector v)
+Vector operator*(const double &r, const Vector &v)
 {
-    return Vector(v.x * r, v.y * r, v.z * r);
+    return v*r;
 }
 
-//POST: returns the Euclidean norm ||v|| of the vector v
-double norm(const Vector& v)
+//Returns the length of the vector
+double norm(const Vector &v)
 {
-    return sqrt(v*v);
+    return std::sqrt(v.x*v.x+v.y*v.y+v.z*v.z);
 }
 
-//POST: divides a Vector with a scalar
-Vector operator/(Vector v, double r)
+//Returns the normalized Vector e.g. an Vector with the same direction, but length == 1
+Vector normalize(const Vector &v)
 {
-    return Vector(v.x/r, v.y/r, v.z/r);
+    double n = norm(v);
+    return Vector(v.x/n, v.y/n, v.z/n);
 }
 
-//PRE: non-zero vector v
-//POST: returns the normalized vector, ||v||=1
-Vector normalize(const Vector& v)
-{
-    assert(v*v != 0);
+//Part b
 
-    return v/norm(v);
-}
-
-//PART B
 struct Ray
 {
     Vector start;
-    Vector direction; //INV: normalized
+    Vector direction;
 
-    //Constructor
-    //PRE: non-zero vector d
-    //POST: ray with offset s and normalized direction vector d
-    Ray(Vector s, Vector d) :
-        start(s), direction(normalize(d)) {};
+    Ray(Vector s, Vector d)
+    {
+        start = s;
+        direction = normalize(d);
+    }
 };
 
 struct Shape
 {
-    int type;               // INV: either 0 (sphere) or 1 (plane)
-    Vector v;               // sphere: center, plane: plane normal
-    double d;               // sphere: radius, plane: distance from origin
-    Color ambientColor;     // ambient color ("Eigenleuchten")
-    Color diffuseColor;     // diffuse color ("Diffuse Lichtreflektion")
-    Color reflectionColor;  // reflection color ("Spiegelung")
+    int type;//0 = sphere, 1 = plane
+    Vector v;//sphere: center, plane: plane normal
+    double d;//sphere: radius, plane: distance from origin
 
-    // POST: shape is initialized with type = Type, v = V
-    // d = D, ambientColor = aC, diffuseColor=dC and
-    // reflectionColor = rC
-    Shape(int Type, Vector V, double D, Color aC, Color dC, Color rC) :
-        type(Type), v(type == 1 ? normalize(V) : V), d(D), ambientColor(aC), diffuseColor(dC), reflectionColor(rC) {};
+    Color ambientColor;
+    Color diffuseColor;
+    Color reflectionColor;
 
-    // POST: if there is an intersection between ray and
-    // this shape, return true and return intersection
-    // point as the parameter t in ray.start + ray.direction * t.
-    // return false otherwise
-    bool intersect(const Ray& ray, double& t) const
+    Shape(int Type, Vector V, double D, Color aC, Color dC, Color rC)
+    {
+        type = Type;
+        if(type == 1) v = normalize(V);
+        else v = V;
+        d = D;
+        ambientColor = aC;
+        diffuseColor = dC;
+        reflectionColor = rC;
+    }
+
+    bool intersect(const Ray& ray, double& t)const
     {
         double t1, t2;
         if(type == 0)
         {
-            //Type is sphere
-            Vector dv = ray.start - v;
-            double discriminant = (ray.direction*dv)*(ray.direction*dv) - dv*dv + d*d;
-            if(discriminant < 0.0)
-            {
-                //no intersection
-                return false;
-            }
+            Vector dv = ray.start-v;//delta v
+            double discriminant = (ray.direction * dv) * (ray.direction * dv) - (dv * dv) + d * d;
 
-            t1 = -(dv*ray.direction) + sqrt(discriminant);
-            t2 = -(dv*ray.direction) - sqrt(discriminant);
+            if(discriminant < 0) return false;//No intersection
+
+            t1 = (-dv * ray.direction) + std::sqrt(discriminant);
+            t2 = (-dv * ray.direction) - std::sqrt(discriminant);
         }
         else
         {
-            //Don't need it
-            t2 = -1;
-
-            t1 = (d - ray.start*v) / (ray.direction*v);
+            t2 = -1;//it's a plane, only one intersection possible, therefore isn't needed anymore
+            t1 = (d - ray.start * v) / (ray.direction * v);
         }
 
-        if(std::min(t1, t2) < 0)
-        {
-            if(std::max(t1, t2) < 0)
-            {
-                //t1 and t2 are smaller than zero so ignore it
-                return false;
-            }
+        if(t1 < 0 && t2 < 0) return false;//both are smaller zero, aren't needed
 
-            t = std::max(t1, t2);
-            return true;
-        }
-
-        t = std::min(t1, t2);
+        if((t1 < 0 && t2 > 0) || t2 < t1) t = t2;
+        if((t2 < 0 && t1 > 0) || (t1 < t2 && t1 > 0)) t = t1;
         return true;
     }
 
-    // PRE: vector "at" lies on the surface of this object
-    // POST: return a normal vector of the shape at position "at"
     Vector get_normal(const Vector& at) const
     {
-        return type == 0 ? normalize(at-v) : v;
+        if(type == 1) return normalize(v);
+        else return normalize(at-v);
     }
 };
 
-struct Scene
-{
-    Vector cameraVector;        // camera position
-    Vector lightVector;         // light position
-    Color lightColor;           // light color
-    unsigned int plane_width;   // observation window width
-    unsigned int plane_height;  // observation window height
-    std::vector<Shape> shapes;  // vector of shapes
+//Part c
 
-    //POST: scene is initialized with cameraVector = cV, lightVector lV,
-    // lightColor lC, plane_width w, plane_height h and
-    // and empty vector shapes
-    Scene(Vector cV, Vector lV, Color lC, int w, int h) :
-        cameraVector(cV), lightVector(lV), lightColor(lC), plane_width(w), plane_height(h) {};
+struct Scene{
+    Vector cameraVector; //Camera position
+    Vector lightVector; //light position
+    Color lightColor; //light color
+    unsigned int plane_width; //observation windows width
+    unsigned int plane_height; //observation window height
+    std::vector<Shape> shapes; //vector of shapes
 
-    // POST: shape is added to the list of shapes
-    void add_shape(const Shape& shape)
-    {
+    Scene(Vector cV, Vector lV, Color lC, int w, int h){
+        cameraVector = cV;
+        lightVector = lV;
+        lightColor = lC;
+        plane_width = w;
+        plane_height = h;
+    }
+
+    void add_shape(const Shape& shape){
         shapes.push_back(shape);
     }
 
-    // POST: Return pointer to shape that has closest intersection with ray, if any
-    // return null otherwise
-    // t contains the parameter of the ray for the intersection point, if any
-    const Shape* intersect(const Ray& ray, double& t) const
-    {
-        double t0 = +INFINITY;
+    const Shape* intersect(const Ray& ray, double& t) const{
+        double t0 = std::numeric_limits<double>::max();
         const Shape* s0 = NULL;
-
-        int len = shapes.size();
-        for(int i = 0; i < len; ++i)
-        {
-            if(shapes[i].intersect(ray, t) && t < t0)
-            {
+        for(int i = 0; i < shapes.size(); i++){
+            if(shapes[i].intersect(ray, t) && t < t0){
                 s0 = &shapes[i];
                 t0 = t;
             }
         }
-
         t = t0;
         return s0;
     }
 
-    //POST: return a ray from camera origin through x,y on the pixel grid
-    Ray create_ray(int x, int y) const
-    {
-        return Ray(cameraVector, Vector((2.0 * x + 1.0 - plane_width) / (2.0 * plane_width), (-2.0 * y -1.0 + plane_height) / (2.0 * plane_height), 0) - cameraVector);
+    //Part d
+    Ray create_ray(int x, int y) const{
+        Vector td = Vector((2.0*x+1.0-plane_width)/(2.0*plane_width), (-2.0*y-1.0+plane_height)/(2.0*plane_height), 0);
+        return Ray(cameraVector, td - cameraVector);//d - cameraVector means direction from cameravector to the pixel with the coordinates of d
     }
 
-    //POST: return color returned by ray
-    Color trace_ray(const Ray& ray, const int depth) const
-    {
+    //Part e
+    Color trace_ray(const Ray& ray, const int depth) const{
         Color c = Black;
-        const Shape* s = NULL;
+        const Shape* shape = NULL;
         double t = 0;
+        shape = intersect(ray, t);
 
-        s = intersect(ray, t);
+        if(shape != NULL){
 
-        if(s != NULL)
-        {
-            // (i) ambient light
-            c = c + s->ambientColor;
+            //(i)
+            c = c + shape->ambientColor;
 
-            // (ii) diffuse light and shadow
-            Vector rT = ray.start + t * ray.direction;
-            Vector v = lightVector - rT;
-            Ray sR = Ray(rT, v);
-            sR.start += v/1000; //hint b
+            //(ii)
+            Vector Rt = ray.start + t*ray.direction;
+            Vector v = lightVector-Rt;  //Shadow ray direction
 
-            const Shape* t = NULL;
+            Ray sR = Ray(Rt, v);
+            sR.start += normalize(v)*(1.0/1000);//hint
+
+            const Shape* T = NULL;
             double u = 0;
-            t = intersect(sR, u);
+            T = intersect(sR, u);
 
-            bool illuminated = (t == NULL) || (u > norm(v));
+            bool illuminated = false;
+            if(T == NULL || u > norm(v)) illuminated = true;
 
-            if(illuminated)
-            {
-                Vector hN = normalize(s->get_normal(rT));
-                c += lightColor*s->diffuseColor*(sR.direction*hN);
+            if(illuminated){
+                Vector hN = normalize(shape->get_normal(Rt));
+                c = c + lightColor*shape->diffuseColor*(sR.direction*hN);
             }
 
-            // (iii) specular highlighting
-            Ray rR = Ray(rT, ray.direction - (2.0 * (s->get_normal(rT) * ray.direction) * s->get_normal(rT)));
-            rR.start += rR.direction/1000;
+            //(iii)
+            Ray rR = Ray(Rt, ray.direction-(2.0*(shape->get_normal(Rt)*ray.direction)*shape->get_normal(Rt)));//hint
+            rR.start += normalize(rR.direction)*(1.0/1000);
 
-            if(illuminated)
-            {
-                if(rR.direction * sR.direction > 0)
-                {
-                    c += lightColor * s->reflectionColor * pow(rR.direction*sR.direction, 16.0);
-                }
+            if(illuminated){
+                if(rR.direction*sR.direction > 0)
+                    c = c + lightColor * shape->reflectionColor * std::pow(rR.direction*sR.direction,16.0);
             }
 
-            //(iv) reflection recursion
-            if(depth > 0)
-            {
-                Color rC = trace_ray(rR, depth - 1);
-                c += rC * s->reflectionColor;
+            //(iv)
+            if(depth > 0){
+                Color rC = trace_ray(rR, depth-1);
+                c = c + rC * shape->reflectionColor;
             }
         }
 
         return c;
+
     }
 
-    //POST: run raytracer on bitmap
-    void raytrace(Bitmap& bitmap, const int depth) const
-    {
-        for(int x = 0; x < plane_width; ++x)
-        {
-            for(int y = 0; y < plane_height; ++y)
-            {
-                bitmap(x,y) = trace_ray(create_ray(x, y), depth);
+    void raytrace(Bitmap& bitmap, const int depth) const{
+        for(int i = 0; i < plane_width; ++i){
+            for(int j = 0; j < plane_height; ++j){
+                bitmap(i, j) = trace_ray(create_ray(i, j), depth);
             }
         }
     }
+
+
 };
